dfb7ac0d0690edfb78d4188daff66b934e5cbea7
Ruslan Pomorov <r.pomorov@gmail.com>
2019-01-23 15:19:04 +0200

    Tests

---
+++ tests/EntityTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\AEntityComment;
+use App\Entity\Article;
+use App\Entity\Category;
+use PHPUnit\Framework\TestCase;
+
+class EntityTest extends TestCase
+{
+    /**
+     * @dataProvider provideAttributes
+     */
+    public function testSomething($attribute, $class)
+    {
+        $this->assertClassHasAttribute($attribute, $class);
+    }
+
+    public function provideAttributes()
+    {
+        return [
+            ['name', Category::class],
+            ['description', Category::class],
+            ['name', Article::class],
+            ['content', Article::class],
+            ['file', Article::class],
+            ['author', AEntityComment::class],
+            ['content', AEntityComment::class],
+            ['created_at', AEntityComment::class],
+        ];
+    }
+}
---
+++ tests/PageTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PageTest extends WebTestCase
+{
+    /**
+     * @dataProvider provideUrls
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function provideUrls()
+    {
+        return [
+            ['/'],
+            ['/category/'],
+            ['/article/'],
+            ['/category/new/'],
+            ['/article/new/'],
+        ];
+    }
+
+    /**
+     * @dataProvider provideMainPageLinks
+     */
+    public function testMainPageLinks($linkName)
+    {
+        $client = self::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $linkCount = $crawler
+            ->filter('a:contains("' . $linkName . '")')
+            ->count()
+        ;
+
+        $this->assertEquals(1, $linkCount);
+
+        $client->clickLink($linkName);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function provideMainPageLinks()
+    {
+        return [
+            ['Categories'],
+            ['Articles'],
+        ];
+    }
+}
